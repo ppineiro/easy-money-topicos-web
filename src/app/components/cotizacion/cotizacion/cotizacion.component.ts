c5d06dcd8935717fceea5396e1f5e157d78487d1
teoalpuy19 <alpuy.mateo@gmail.com>
2019-10-31 20:38:58 -0300

    arregle todo lo que @pablosaravia29 habia hecho mal y lo subi

--- src/app/components/cotizacion/cotizacion/cotizacion.component.ts
+++ src/app/components/cotizacion/cotizacion/cotizacion.component.ts
@@ -1,36 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import {BrouCotService} from 'src/app/services/broucot.service';
-import {DivisaBrouModel} from 'src/app/services/models/divisabrou.model';
+import { Component, OnInit } from "@angular/core";
+import { BrouCotService } from "src/app/services/broucot.service";
+import { DivisaBrouModel } from "src/app/services/models/divisabrou.model";
 
 @Component({
-  selector: 'app-cotizacion',
-  templateUrl: './cotizacion.component.html',
-  styleUrls: ['./cotizacion.component.css']
+  selector: "app-cotizacion",
+  templateUrl: "./cotizacion.component.html",
+  styleUrls: ["./cotizacion.component.css"]
 })
 export class CotizacionComponent {
   resultado = [];
   data: DivisaBrouModel[] = [];
-  
+
   constructor(private service: BrouCotService) {
     this.getData();
   }
- 
+
   getData() {
     this.service.getCotizacion().subscribe(eventos => {
       console.log(eventos);
-      this.data = this.sustituirIntegracionesPorValores(eventos);
-    /*  console.log(this.data);*/
+      console.log("entre " + eventos.rates.USD.sell);
+      let rates = eventos["rates"];
+      let peso = rates[0];
+
+      // this.data = this.sustituirIntegracionesPorValores(eventos);
+      /*  console.log(this.data);*/
       return this.data;
     });
   }
 
-  sustituirIntegracionesPorValores(array: DivisaBrouModel[]): any[] {
+  sustituirIntegracionesPorValores(array: any[]): any[] {
     for (let index = 0; index < array.length; index++) {
       const element = array[index];
       this.resultado.push(element);
+      console.log("entre");
       this.resultado[index].monto = element.rates.ARS.buy;
       console.log(element);
-      
     }
     return this.resultado;
   }
--- src/app/services/broucot.service.ts
+++ src/app/services/broucot.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { DivisaBrouModel } from "./models/divisabrou.model";
 
-const API_URL = 'https://cotizaciones-brou.herokuapp.com/api/currency/latest';
+const API_URL = "https://cotizaciones-brou.herokuapp.com/api/currency/latest";
 
 @Injectable({
   providedIn: "root"
@@ -13,12 +13,7 @@ export class BrouCotService {
     console.log("Service Listo");
   }
 
-
-
-  getCotizacion(): Observable<DivisaBrouModel[]> {
-    return this.http.get<DivisaBrouModel[]>(API_URL);
+  getCotizacion(): Observable<DivisaBrouModel> {
+    return this.http.get<DivisaBrouModel>(API_URL);
   }
-
-
-
 }
