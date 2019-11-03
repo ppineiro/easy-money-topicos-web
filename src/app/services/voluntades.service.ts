9e548b5dcd4446a4af7e1ec9e64d8d9b1f4a7730
Pablo Saravia <pablodsaravia@hotmail.com>
2019-11-03 11:52:42 -0300

    commit

--- src/app/services/voluntades.service.ts
+++ src/app/services/voluntades.service.ts
@@ -12,7 +12,6 @@ const API_URL = 'http://localhost:8000/voluntades';
 export class VoluntadesService {
   constructor(private http: HttpClient) {
     console.log('Service Listo');
-    console.log('Service ok');
   }
 
   getVoluntades(): Observable<VoluntadModel[]> {
